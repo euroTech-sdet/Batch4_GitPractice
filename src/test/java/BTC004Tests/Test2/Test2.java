193d5611c02a19f29fa297f558ae122ceab4885c
TalhaO <talha43gs@gmail.com>
2022-09-15 18:15:39 +0100

    Test2 Updated

--- src/test/java/BTC004Tests/Test2/Test2.java
+++ src/test/java/BTC004Tests/Test2/Test2.java
@@ -5,7 +5,6 @@ public class Test2 {
     public static void main(String[] args) {
         // code 1
         // verify (exp , act);
-        // verify (exp , act);
         // code 2
 
         // End of my day2
