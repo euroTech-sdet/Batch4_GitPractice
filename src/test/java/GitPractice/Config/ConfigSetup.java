844f9f9c6962f452715a5efbf6f35023275b4eae
TalhaO <talha43gs@gmail.com>
2022-09-15 20:09:08 +0100

    Config Updated

--- src/test/java/GitPractice/Config/ConfigSetup.java
+++ src/test/java/GitPractice/Config/ConfigSetup.java
@@ -3,6 +3,7 @@ package GitPractice.Config;
 public class ConfigSetup {
 private static final int revisionValue= 1001;
 private static final long INVOICE_PREFIX = 10001001;
+public static int testValue= 1111;
 
 
 }
