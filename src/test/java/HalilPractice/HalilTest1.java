bf9a80be13c410877b51026f1a09d2c2ea2dc278
Halil Arslan <harslan7579@gmail.com>
2022-09-15 19:51:31 +0200

    Please check them and confirm then

---
+++ src/test/java/HalilPractice/HalilTest1.java
@@ -0,0 +1,7 @@
+package HalilPractice;
+
+public class HalilTest1 {
+    // Confirm it
+    //after check
+    //please
+}
---
+++ src/test/java/HalilPractice/HalilTest2.java
@@ -0,0 +1,6 @@
+package HalilPractice;
+
+public class HalilTest2 {
+    //correct it
+    //after check
+}
