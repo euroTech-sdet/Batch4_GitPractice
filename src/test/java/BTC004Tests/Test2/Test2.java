2959f122a3a5b6c7712d735cb04fe70531bc6ae9
TalhaO <talha43gs@gmail.com>
2022-09-15 17:33:48 +0100

    Test2 Uploaded

---
+++ src/test/java/BTC004Tests/Test2/Test2.java
@@ -0,0 +1,15 @@
+package BTC004Tests.Test2;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+        // code 1
+        // verify (exp , act);
+        // verify (exp , act);
+        // code 2
+
+        // End of my day2
+    }
+
+
+}
